6f86b724cdd0ac701508cf66f18cf08a3ff11c2d
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-08-26 14:08:27 +0200

    MessagesPage started with design missing final touches

--- src/Pages/MessagesPage/MessagesPage.js
+++ src/Pages/MessagesPage/MessagesPage.js
@@ -1,8 +1,249 @@
 import React from 'react'
+import Newsletter from '../../Components/Main/newsletter/Newsletter'
+import ShippingDetails from '../../Components/Main/ShippingDetails/ShippingDetails'
+import Footer from '../../Sections/Footer'
+import Navbar from '../../Sections/Navbar'
+import './MessagesPage.css'
 
 const MessagesPage = () => {
   return (
-    <div>MessagesPage</div>
+    <div className="MessagesPage">
+      <Navbar/>
+      <div className="MessagesMain">
+        <div className="MessagesMainTop">
+          <div className="MessagesMainTopLeft">MessagesMainTopRight</div>
+          <div className="MessagesMainTopRight">MessagesMainTopLeft</div>
+        </div>
+        <div className="MesagesMainMain">
+          <div className="MesagesMainMainLeft">
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+            <div className='MesagesMainMainLeftUser'>
+              <div className="MesagesMainMainLeftUserIcon">icon</div>
+              <h3 className='MesagesMainMainLeftUserLabel'>Jabu Mahlangu</h3>
+            </div>
+          </div>
+          <div className="MesagesMainMainRight">
+            <div className="MesagesMainMainRightBody">
+              MesagesMainMainRightBody
+            </div>
+            <div className="MesagesMainMainRightTextarea">
+              <div className='MesagesMainMainRightTextareaLeft'>MesagesMainMainRightTextareaLeft</div>
+              <div className='MesagesMainMainRightTextareaRight'>
+                <input type="text" className='MesagesMainMainRightTextareaRightInput'/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="MesagesMainFooter">
+          <div className="MesagesMainFooterTop">
+            <Newsletter/>
+          </div>
+          <div className="MesagesMainFooterBottom">
+            <ShippingDetails/>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
